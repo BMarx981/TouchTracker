bc516a83c11fe3dd823f77370e7094489ce5dee1
Brian Marx <b.marx981@gmail.com>
2017-03-14 16:08:13 -0400

    Line class added

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Marx, Brian on 3/14/17.
+//  Copyright © 2017 Marx, Brian. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
