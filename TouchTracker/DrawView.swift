fdd5442c4f77323c40ac8ce5e04a22c478a91d85
Brian Marx <b.marx981@gmail.com>
2017-03-14 17:28:21 -0400

    Touches Cancelled added

--- TouchTracker/DrawView.swift
+++ TouchTracker/DrawView.swift
@@ -110,4 +110,12 @@ class DrawView: UIView {
         setNeedsDisplay()
     }
     
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(#function)
+        
+        currentLines.removeAll()
+        
+        setNeedsDisplay()
+    }
+    
 }
